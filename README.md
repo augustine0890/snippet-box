781d09c03f9a00eff4e3fd7103c5ab42a587c20a
augustine <augustino0890@gmail.com>
2022-05-03 16:31:54 +0900

    leveled logging

--- README.md
+++ README.md
@@ -19,6 +19,16 @@
 - The `cmd` directory will contain the application specific code for the executable apps in project. The web application which will live under the `cmd/web` directory.
 - The `pkg` directory will contain the non-application code used in the project. It holds potentially reusable code like validation helpers and the SQL database models for the project.
 - The `ui` directory will contain the user-interface assets used by the web app.
+### Logging
+- Prefix informational messages with `"INFO"`
+- Prefix error messages with `"ERROR"`
+- Logging to a File
+  ```go
+  f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666) if err != nil {
+  log.Fatal(err) }
+  defer f.Close()
+  infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+  ```
 
 ## How to run the app
 - `go run ./cmd/web`
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,18 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// flag will be stored in the addr variable at runtime
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	// logger for writing information message
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// logger for writing error messages, use stderr as destination
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -19,7 +25,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Initialize a new http.Server
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
